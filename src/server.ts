c15f42f1664076a1b94cf17bcbeac4dfa7c3a94b
hod25 <hod7112000@gmail.com>
2024-12-13 10:58:08 +0200

    1

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,35 @@
-import dotenv from "dotenv"
+import express, { Express } from "express";
+const app = express();
+import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import express, { Express } from "express";
-import postsRoute from "./routes/posts_route";
-import commentsRoute from "./routes/comments_route";
+import posts_routes from "./routes/posts_route";
+import comments_routes from "./routes/comments_routes";
+import auth_routes from "./routes/auth_routes";
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/posts", postsRoute);
-app.use("/comments", commentsRoute);
-
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to database"));
-
-const initApp = () => {
+const initApp = (): Promise<Express> => {
   return new Promise<Express>((resolve, reject) => {
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", function () {
+      console.log("Connected to the database");
+    });
     if (!process.env.DB_CONNECT) {
-      reject("DB_CONNECT is not defined in .env file");
+      reject("DB_CONNECT is not defined");
     } else {
       mongoose
         .connect(process.env.DB_CONNECT)
         .then(() => {
+          app.use(bodyParser.json());
+          app.use(bodyParser.urlencoded({ extended: true }));
+          app.use("/posts", posts_routes);
+          app.use("/comments", comments_routes);
+          app.use("/auth", auth_routes);
           resolve(app);
         })
-        .catch((error) => {
-          reject(error);
+        .catch((err) => {
+          reject(err);
         });
     }
   });

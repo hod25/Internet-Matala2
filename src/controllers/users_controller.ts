5dfca93e4951b1fe27f5119c63973217e8ab4cfc
hod25 <hod7112000@gmail.com>
2024-12-18 16:59:37 +0200

    --

--- src/controllers/users_controller.ts
+++
@@ -1,6 +0,0 @@
-import userModel from "../models/users_model";
-import BaseController from "./base_controller";
-
-const usersController = new BaseController(userModel);
-
-export default usersController;
